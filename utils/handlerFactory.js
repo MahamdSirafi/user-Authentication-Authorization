490a8c24c30b6879cfac1bc6442a87553b64acb5
MahamdSirafi <116427055+MahamdSirafi@users.noreply.github.com>
2023-11-04 16:13:43 +0300

    Add files via upload

--- utils/handlerFactory.js
+++ utils/handlerFactory.js
@@ -34,24 +34,12 @@ exports.createOne = (Model) =>
       doc,
     });
   });
-exports.getOne = (Model, popOptions) =>
+exports.getOne = (Model, ...popOptions) =>
   catchAsync(async (req, res, next) => {
     let query = Model.findById(req.params.id);
-    if (popOptions) query = query.populate(popOptions);
-    const doc = await query;
-    if (!doc) {
-      return next(new AppError("No document found with that ID", 404));
-    }
-    res.status(200).json({
-      status: "success",
-      doc,
-    });
-  });
-exports.getOne2 = (Model, popOptions, popOptions2) =>
-  catchAsync(async (req, res, next) => {
-    let query = Model.findById(req.params.id);
-    if (popOptions) query = query.populate(popOptions);
-    if (popOptions2) query = query.populate(popOptions2);
+    if (popOptions)
+      for (let i = 0; i < popOptions.length; i++)
+        query = query.populate(popOptions[i]);
     const doc = await query;
     if (!doc) {
       return next(new AppError("No document found with that ID", 404));
@@ -71,9 +59,7 @@ exports.getAll = (Model) =>
             .limitFields()
             .paginate()
         : new APIFeatures(Model, req.query).agg().aggDate();
-    // const doc = await features.query.explain();
     const doc = await features.query;
-    // SEND RESPONSE
     res.status(200).json({
       status: "success",
       results: doc.length,
@@ -85,11 +71,8 @@ exports.getAllpop = (Model, pop) =>
     let fullter = {};
     if (req.params.userId) fullter = { user: req.params.userId };
     let features = new APIFeatures(Model.find(), req.query);
-    // console.log(pop);
     if (pop) {
       let pop2 = pop.split(" ");
-      // console.log(pop2);
-      // console.log(pop,' ++++++++++++++++++++++ ',pop.length);
       for (let i = 0; i < pop2.length; i++) {
         features.query = features.query.populate({
           path: `${pop2[i]}`,
@@ -97,8 +80,6 @@ exports.getAllpop = (Model, pop) =>
       }
     }
     features.filter().sort().limitFields().paginate();
-    // console.log(features.query);
-    // const doc = await features.query.explain();
     const doc = await features.query;
     // SEND RESPONSE
     res.status(200).json({
@@ -108,26 +89,13 @@ exports.getAllpop = (Model, pop) =>
     });
   });
 
-exports.getAllpop1 = (Model, pop) =>
+exports.getAllpop1 = (Model, ...pop) =>
   catchAsync(async (req, res, next) => {
     let features = new APIFeatures(Model.find(), req.query).filter();
     if (pop) {
-      features.query = features.query.populate(pop);
+      for (let i = 0; i < pop.length; i++)
+        features.query = features.query.populate(pop[i]);
     }
-    features.sort().limitFields().paginate();
-    const doc = await features.query;
-    // SEND RESPONSE
-    res.status(200).json({
-      status: "success",
-      results: doc.length,
-      doc,
-    });
-  });
-exports.getAllpop2 = (Model, pop, pop2) =>
-  catchAsync(async (req, res, next) => {
-    let features = new APIFeatures(Model.find(), req.query).filter();
-    if (pop) features.query = features.query.populate(pop);
-    if (pop2) features.query = features.query.populate(pop2);
     features.sort().limitFields().paginate();
     const doc = await features.query;
     // SEND RESPONSE
